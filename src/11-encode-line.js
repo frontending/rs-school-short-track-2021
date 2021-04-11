1097255bbf27fc78999b9520a0021638f14f5d62
frontending <mymage259@gmail.com>
2021-04-12 00:54:03 +0300

    feat: complete task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,22 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let count = 1;
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else {
+      if (count === 1) {
+        result += str[i];
+      } else {
+        result = result + count.toString() + str[i];
+      }
+      count = 1;
+    }
+  }
+  return result;
 }
 
 module.exports = encodeLine;
